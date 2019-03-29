873d27496e9aa140b6573d483440c83e4f1547fa
f14r3 <f14r33@gmail.com>
2019-03-29 22:54:57 +0000

    add SafeMath for Add, Sub and replace most assert with require

--- property.py
+++ property.py
@@ -10,6 +10,48 @@ from ontology.interop.Ontology.Runtime import Base58ToAddress
 from ontology.builtins import concat, len, append, remove
 
 
+############### RequireWitness (safetycheck) Defination Starts #################
+def Revert():
+    raise Exception(0xF1F1F2F2F3F3F4F4)
+
+def Require(condition):
+    if not condition:
+        Revert()
+    return True
+
+def RequireWitness(witness):
+    """
+    Checks the tx sender is equal to the witness. If not
+    satisfying, revert the transaction.
+    """
+    Require(CheckWitness(witness))
+    return True
+############### RequireWitness (safetycheck) Defination Ends ###################
+
+########################### SafeMath Defination Starts ##########################
+"""
+https://github.com/ONT-Avocados/python-template/blob/master/libs/SafeMath.py
+"""
+def Add(a, b):
+    """
+	Adds two numbers, throws on overflow.
+	"""
+    c = a + b
+    Require(c >= a)
+    return c
+
+def Sub(a, b):
+    """
+    Substracts two numbers, throws on overflow (i.e. if subtrahend is greater than minuend).
+    :param a: operand a
+    :param b: operand b
+    :return: a - b if a - b > 0 or revert the transaction.
+    """
+    Require(a>=b)
+    return a-b
+
+########################### SafeMath Defination Ends ###########################
+
 
 TransferEvent = RegisterAction("transfer", "fromAcct", "toAcct", "tokenId", "amount")
 ApprovalEvent = RegisterAction("approval", "owner", "spender", "tokenId", "amount")
@@ -19,8 +61,7 @@ CEO_ADDRESS_KEY = "CEO"
 CTO_ADDRESS_KEY = "CTO"
 COO_ADDRESS_KEY = "COO"
 AUTHORIZED_ADDRESS_LIST_KEY = "AuthorizedAddress"
-CEOAddress = Base58ToAddress('AQf4Mzu1YJrhz9f3aRkkwSm9n3qhXGSh4p')
-
+CEOAddress = Base58ToAddress('ceoxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx')
 
 CONTRACT_PAUSED_KEY = "Pause"
 # NAME_PREFIX + tokenId --- to store the name of the tokenId token
@@ -38,24 +79,29 @@ TOTAL_SUPPLY_PREFIX = 'TotalSupply'
 def Main(operation, args):
     ########################## Methods that meet the OEP8 protocal Starts ############################
     if operation == "name":
-        assert (len(args) == 1)
+        if len(args) != 1:
+            return False
         tokenId = args[0]
         return name(tokenId)
     if operation == "symbol":
-        assert (len(args) == 1)
+        if len(args) != 1:
+            return False
         tokenId = args[0]
         return symbol(tokenId)
     if operation == "totalSupply":
-        assert (len(args) == 1)
+        if len(args) != 1:
+            return False
         tokenId = args[0]
         return totalSupply(tokenId)
     if operation == "balanceOf":
-        assert (len(args) == 2)
-        acct = args[0]
+        if len(args) != 2:
+            return False
+        account = args[0]
         tokenId = args[1]
-        return balanceOf(acct, tokenId)
+        return balanceOf(account, tokenId)
     if operation == "transfer":
-        assert (len(args) == 4)
+        if len(args) != 4:
+            return False
         fromAcct = args[0]
         toAcct = args[1]
         tokenId = args[2]
@@ -64,7 +110,8 @@ def Main(operation, args):
     if operation == "transferMulti":
         return transferMulti(args)
     if operation == "approve":
-        assert (len(args) == 4)
+        if len(args) != 4:
+            return False
         owner = args[0]
         spender = args[1]
         tokenId = args[2]
@@ -73,13 +120,15 @@ def Main(operation, args):
     if operation == "approveMulti":
         return approveMulti(args)
     if operation == "allowance":
-        assert (len(args) == 3)
+        if len(args) != 3:
+            return False
         owner = args[0]
         spender = args[1]
         tokenId = args[2]
         return allowance(owner, spender, tokenId)
     if operation == "transferFrom":
-        assert (len(args) == 5)
+        if len(args) != 5:
+            return False
         spender = args[0]
         fromAcct = args[1]
         toAcct = args[2]
@@ -91,20 +140,24 @@ def Main(operation, args):
     ########################## Methods that meet the OEP8 protocal Ends ############################
     #################### Special methods for CEO only defination starts  ######################
     if operation == "setCLevel":
-        assert (len(args) == 2)
+        if len(args) != 2:
+            return False
         option = args[0]
         account = args[1]
         return setCLevel(option, account)
     if operation == "setAuthorizedLevel":
-        assert (len(args) == 1)
+        if len(args) != 1:
+            return False
         account = args[0]
         return setAuthorizedLevel(account)
     if operation == "removeAuthorizedLevel":
-        assert (len(args) == 1)
+        if len(args) != 1:
+            return False
         account = args[0]
         return removeAuthorizedLevel(account)
     if operation == "createToken":
-        assert (len(args) == 3)
+        if len(args) != 3:
+            return False
         tokenId = args[0]
         name = args[1]
         symbol = args[2]
@@ -120,7 +173,8 @@ def Main(operation, args):
     ############# Special methods for C Level accounts only defination Ends  ################
     ############# Special methods for C Level and authorized level defination Starts  ################
     if operation == "mintToken":
-        assert (len(args) == 3)
+        if len(args) != 3:
+            return False
         toAcct = args[0]
         tokenId = args[1]
         amount = args[2]
@@ -128,7 +182,8 @@ def Main(operation, args):
     if operation == "multiMintToken":
         return multiMintToken(args)
     if operation == "burnToken":
-        assert (len(args) == 3)
+        if len(args) != 3:
+            return False
         account = args[0]
         tokenId = args[1]
         amount = args[2]
@@ -189,29 +244,29 @@ def transfer(fromAcct, toAcct, tokenId, amount):
     :param amount:
     :return:
     """
-    assert (_whenNotPaused())
-    assert (CheckWitness(fromAcct))
-    assert (_tokenExist(tokenId))
-    assert (len(fromAcct) == 20)
-    assert (len(toAcct) == 20)
+    Require(_whenNotPaused())
+    RequireWitness(fromAcct)
+    Require(_tokenExist(tokenId))
+    Require(len(fromAcct) == 20)
+    Require(len(toAcct) == 20)
 
     balanceKey = _concatkey(BALANCE_PREFIX, tokenId)
     fromKey = _concatkey(balanceKey, fromAcct)
     fromBalance = Get(GetContext(), fromKey)
 
-    # if amount > fromBalance or amount <= 0:
-    #     return False
+    if amount > fromBalance or amount <= 0:
+        return False
 
-    assert (amount <= fromBalance and amount > 0)
+    # assert (amount <= fromBalance and amount > 0)
 
     if amount == fromBalance:
         Delete(GetContext(), fromKey)
     else:
-        Put(GetContext(), fromKey, fromBalance - amount)
+        Put(GetContext(), fromKey, Sub(fromBalance, amount))
 
     toKey = _concatkey(balanceKey, toAcct)
     toBalance = Get(GetContext(), toKey)
-    Put(GetContext(), toKey, toBalance + amount)
+    Put(GetContext(), toKey, Add(toBalance, amount))
 
     TransferEvent(fromAcct, toAcct, tokenId, amount)
 
@@ -225,8 +280,10 @@ def transferMulti(args):
     :return: True or raise exception
     """
     for p in args:
-        assert (len(p) == 4)
-        assert (transfer(p[0], p[1], p[2], p[3]))
+        if len(p) != 4:
+            raise Exception('transferMulti failed - input error!')
+        if transfer(p[0], p[1], p[2], p[3]) == False:
+            raise Exception('transferMulti failed - transfer error!')
     return True
 
 
@@ -239,17 +296,17 @@ def approve(owner, spender, tokenId, amount):
     :param amount:
     :return:
     """
-    assert (_whenNotPaused())
+    Require(_whenNotPaused())
     # make sure the invoker is the owner address
-    assert (CheckWitness(owner))
+    RequireWitness(owner)
     # make sure the address is legal
-    assert (len(spender) == 20)
-    assert (_tokenExist(tokenId))
+    Require(len(spender) == 20)
+    Require(_tokenExist(tokenId))
 
     ownerBalance = balanceOf(owner, tokenId)
     # you can use "if" to notify the corresponding message, or use assert to raise exception
-    assert (ownerBalance >= amount)
-    assert (amount > 0)
+    Require(ownerBalance >= amount)
+    Require(amount > 0)
     key = _concatkey(_concatkey(_concatkey(APPROVE_PREFIX, tokenId), owner), spender)
     Put(GetContext(), key, amount)
 
@@ -266,8 +323,10 @@ def approveMulti(args):
     :return:
     """
     for p in args:
-        assert (len(p) == 4)
-        assert (approve(p[0], p[1], p[2], p[3]))
+        if len(p) != 4:
+            raise Exception('approveMulti failed - input error!')
+        if approve(p[0], p[1], p[2], p[3]) == False:
+            raise Exception('approveMulti failed - approve error!')
     return True
 
 
@@ -290,32 +349,36 @@ def transferFrom(spender, fromAcct, toAcct, tokenId, amount):
     :param amount: False or True
     :return:
     """
-    assert (_whenNotPaused())
-    assert (CheckWitness(spender))
-    assert (len(fromAcct) == 20)
-    assert (len(toAcct) == 20)
-    assert (_tokenExist(tokenId))
+    Require(_whenNotPaused())
+    RequireWitness(spender)
+    Require(len(fromAcct) == 20)
+    Require(len(toAcct) == 20)
+    #RequireScriptHash(spender)
+    #RequireScriptHash(fromAcct)
+    #RequireScriptHash(toAcct)
+    Require(_tokenExist(tokenId))
 
     fromKey = _concatkey(_concatkey(BALANCE_PREFIX, tokenId), fromAcct)
     fromBalance = Get(GetContext(), fromKey)
-    assert (fromBalance >= amount)
-    assert (amount > 0)
+    Require(fromBalance >= amount)
+    Require(amount > 0)
     toKey = _concatkey(_concatkey(BALANCE_PREFIX, tokenId), toAcct)
 
 
     approvedKey = _concatkey(_concatkey(_concatkey(APPROVE_PREFIX, tokenId), fromAcct), spender)
     approvedAmount = Get(GetContext(), approvedKey)
 
-    assert (approvedAmount >= amount)
-    if amount == approvedAmount:
+    if amount > approvedAmount:
+        raise Exception('you are not allowed to withdraw that amount')
+    elif amount == approvedAmount:
         Delete(GetContext(), approvedKey)
-        Put(GetContext(), fromKey, fromBalance - amount)
+        Put(GetContext(), fromKey, Sub(fromBalance, amount))
     else:
-        Put(GetContext(), approvedKey, approvedAmount - amount)
-        Put(GetContext(), fromKey, fromBalance - amount)
+        Put(GetContext(), approvedKey, Sub(approvedAmount, amount))
+        Put(GetContext(), fromKey, Sub(fromBalance, amount))
 
     toBalance = Get(GetContext(), toKey)
-    Put(GetContext(), toKey, toBalance + amount)
+    Put(GetContext(), toKey, Add(toBalance, amount))
 
     TransferEvent(fromAcct, toAcct, tokenId, amount)
 
@@ -329,8 +392,10 @@ def transferFromMulti(args):
     :return:
     """
     for p in args:
-        assert (len(p) == 5)
-        assert (transferFrom(p[0], p[1], p[2], p[3], p[4]))
+        if len(p) != 5:
+            raise Exception('transferFromMulti failed - input error!')
+        if transferFrom(p[0], p[1], p[2], p[3], p[4]) == False:
+            raise Exception('transferFromMulti failed - transfer error!')
     return True
 ########################## Methods that meet the OEP8 protocal Ends ############################
 
@@ -343,7 +408,7 @@ def setCLevel(option, account):
     :param account: the account to be set as the CLevel account
     :return:
     """
-    assert (CheckWitness(CEOAddress))
+    RequireWitness(CEOAddress)
     CLevelKey = None
     if option == "CTO":
         CLevelKey = CTO_ADDRESS_KEY
@@ -354,8 +419,8 @@ def setCLevel(option, account):
     return True
 
 def setAuthorizedLevel(account):
-    assert (CheckWitness(CEOAddress))
-    assert (len(account) == 20)
+    RequireWitness(CEOAddress)
+    Require(len(account) == 20)
     authorizedAddressList = []
     authorizedAddressListInfo = Get(GetContext(), AUTHORIZED_ADDRESS_LIST_KEY)
     if not authorizedAddressListInfo:
@@ -372,8 +437,8 @@ def setAuthorizedLevel(account):
     return True
 
 def removeAuthorizedLevel(account):
-    assert (CheckWitness(CEOAddress))
-    assert (len(account) == 20)
+    RequireWitness(CEOAddress)
+    Require(len(account) == 20)
     authorizedAddressListInfo = Get(GetContext(), AUTHORIZED_ADDRESS_LIST_KEY)
     assert (authorizedAddressListInfo)
     authorizedAddressList = Deserialize(authorizedAddressListInfo)
@@ -388,9 +453,9 @@ def removeAuthorizedLevel(account):
 
 
 def createToken(tokenId, name, symbol):
-    assert (CheckWitness(CEOAddress))
-    assert (not _tokenExist(tokenId))
-    assert (_checkLegalTokenId(tokenId))
+    RequireWitness(CEOAddress)
+    Require(not _tokenExist(tokenId))
+    Require(_checkLegalTokenId(tokenId))
     # Put name and symbol in storage
     Put(GetContext(), _concatkey(NAME_PREFIX, tokenId), name)
     Put(GetContext(), _concatkey(SYMBOL_PREFIX, tokenId), symbol)
@@ -407,8 +472,8 @@ def multiCreateToken(args):
     :return:
     """
     for p in args:
-        assert (len(p) == 3)
-        assert (createToken(p[0], p[1], p[2]))
+        Require(len(p) == 3)
+        Require(createToken(p[0], p[1], p[2]))
     return True
 
 #################### Special methods for CEO only defination Ends  ######################
@@ -416,13 +481,13 @@ def multiCreateToken(args):
 
 ############# Special methods for C Level accounts only defination Starts  ################
 def pause():
-    assert (_onlyCLevel())
+    Require(_onlyCLevel())
     Put(GetContext(), CONTRACT_PAUSED_KEY, "T")
     Notify(["pause"])
     return True
 
 def unpause():
-    assert (_onlyCLevel())
+    Require(_onlyCLevel())
     Put(GetContext(), CONTRACT_PAUSED_KEY, "F")
     Notify(["unpause"])
     return True
@@ -431,20 +496,20 @@ def unpause():
 
 ############# Special methods for C Level and authorized level defination Starts  ################
 def mintToken(toAcct, tokenId, amount):
-    assert (_whenNotPaused())
+    Require(_whenNotPaused())
     assert (_onlyCLevel() or _onlyAuthorizedLevel())
     # make sure the to address is legal
-    assert (len(toAcct) == 20)
+    Require(len(toAcct) == 20)
     # make sure the tokenId has been created already
-    assert (_tokenExist(tokenId))
+    Require(_tokenExist(tokenId))
     # make sure the amount is legal, which is greater than ZERO
-    assert (amount > 0)
+    Require(amount > 0)
     # update the to account balance
-    Put(GetContext(), _concatkey(_concatkey(BALANCE_PREFIX, tokenId), toAcct), balanceOf(toAcct, tokenId) + amount)
+    Put(GetContext(), _concatkey(_concatkey(BALANCE_PREFIX, tokenId), toAcct), Add(balanceOf(toAcct, tokenId), amount))
     # update the total supply
-    Put(GetContext(), _concatkey(TOTAL_SUPPLY_PREFIX, tokenId), totalSupply(tokenId) + amount)
+    Put(GetContext(), _concatkey(TOTAL_SUPPLY_PREFIX, tokenId), Add(totalSupply(tokenId), amount))
     # make sure the total supply is not greater than 1 billion
-    assert (totalSupply(tokenId) <= 10000000000)
+    Require(totalSupply(tokenId) <= 10000000000)
     # Notify the event to the block chain
     TransferEvent("", toAcct, tokenId, amount)
     # Notify(["transfer", "", toAcct, tokenId, amount])
@@ -452,37 +517,39 @@ def mintToken(toAcct, tokenId, amount):
 
 def multiMintToken(args):
     for p in args:
-        assert (len(p) == 3)
-        assert (mintToken(p[0], p[1], p[2]))
+        Require(len(p) == 3)
+        Require(mintToken(p[0], p[1], p[2]))
     return True
 
 def burnToken(account, tokenId, amount):
-    assert (_whenNotPaused())
-    assert (_onlyCLevel())
+    Require(_whenNotPaused())
+    Require(_onlyCLevel())
     # make sure the tokenId has been created already
-    assert (_tokenExist(tokenId))
+    Require(_tokenExist(tokenId))
     # make sure the amount is legal, which is greater than ZERO
     balance = balanceOf(account, tokenId)
-    assert (amount > 0 and amount <= balance)
+    #assert (amount > 0 and amount <= balance)
+    Require(amount <= balance)
+    Require(amount > 0)
     # update the to account balance
     if amount == balance:
         Delete(GetContext(), _concatkey(_concatkey(BALANCE_PREFIX, tokenId), account))
     else:
-        Put(GetContext(), _concatkey(_concatkey(BALANCE_PREFIX, tokenId), account), balance - amount)
+        Put(GetContext(), _concatkey(_concatkey(BALANCE_PREFIX, tokenId), account), Sub(balance, amount))
     # update the total supply
     _totalSupply = totalSupply(tokenId)
     if _totalSupply == amount:
         Delete(GetContext(), _concatkey(TOTAL_SUPPLY_PREFIX, tokenId))
     else:
-        Put(GetContext(), _concatkey(TOTAL_SUPPLY_PREFIX, tokenId), _totalSupply - amount)
+        Put(GetContext(), _concatkey(TOTAL_SUPPLY_PREFIX, tokenId), Sub(_totalSupply, amount))
     # Notify the event to the block chain
     TransferEvent(account, "", tokenId, amount)
     return True
 
 def multiBurnToken(args):
     for p in args:
-        assert (len(p) == 3)
-        assert (burnToken(p[0], p[1], p[2]))
+        Require(len(p) == 3)
+        Require(burnToken(p[0], p[1], p[2]))
     return True
 ############# Special methods for C Level and authorized level defination Ends  ################
 
@@ -566,7 +633,7 @@ def _findInList(e, l):
     for eInl in l:
         if eInl == e:
             return index
-        index = index + 1
+        index = Add(index, 1)
     return index
 
 
