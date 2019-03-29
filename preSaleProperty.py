83da7ce8cd7899f09e1bd72d81b49bba282dd19b
f14r3 <f14r33@gmail.com>
2019-03-29 13:51:47 +0000

    replace assert with Require for safety concern

--- preSaleProperty.py
+++ preSaleProperty.py
@@ -12,8 +12,8 @@ from ontology.libont import AddressFromVmCode
 ONTAddress = bytearray(b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x01')
 ONGAddress = bytearray(b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x02')
 
-Admin = Base58ToAddress("AQf4Mzu1YJrhz9f3aRkkwSm9n3qhXGSh4p")
-PreSaleReceiver = Base58ToAddress("AQf4Mzu1YJrhz9f3aRkkwSm9n3qhXGSh4p")
+Admin = Base58ToAddress("ceoxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
+PreSaleReceiver = Base58ToAddress("vaultxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
 SelfContractAddress = GetExecutingScriptHash()
 # GP means gift package
 GP_PREFIX = "GPContent"
@@ -22,21 +22,40 @@ PROPERTY_REVERSED_HASH_KEY = "PropertyHash"
 PRESALE_PAUSED_KEY = "Pause"
 GP_MAX_PER_TX_KEY = "GPMaxPerTx"
 
+############### RequireWitness (safetycheck) Defination Starts #################
+def Revert():
+    raise Exception(0xF1F1F2F2F3F3F4F4)
+
+def Require(condition):
+    if not condition:
+        Revert()
+    return True
+
+def RequireWitness(witness):
+    """
+    Checks the tx sender is equal to the witness. If not
+    satisfying, revert the transaction.
+    """
+    Require(CheckWitness(witness))
+    return True
+############### RequireWitness (safetycheck) Defination Ends ###################
+
+
 def Main(operation, args):
     ############# Methods for Admin account only defination Starts  ################
     if operation == "setPropertyHash":
-        assert (len(args) == 1)
+        Require(len(args) == 1)
         propertyReversedHash = args[0]
         return setPropertyHash(propertyReversedHash)
     if operation == "setGP":
-        assert (len(args) == 4)
+        Require(len(args) == 4)
         gpId = args[0]
         gpLimit = args[1]
         price = args[2]
         gpContent = args[3]
         return setGP(gpId, gpLimit, price, gpContent)
     if operation == "setGPMaxPerTx":
-        assert (len(args) == 1)
+        Require(len(args) == 1)
         limit = args[0]
         return setGPMaxPerTx(limit)
     if operation == "withdraw":
@@ -46,7 +65,7 @@ def Main(operation, args):
     if operation == "unpause":
         return unpause()
     if operation == "migrateContract":
-        assert (len(args) == 7)
+        Require(len(args) == 7)
         code = args[0]
         needStorage = args[1]
         name = args[2]
@@ -58,7 +77,7 @@ def Main(operation, args):
     ############# Methods for Admin account only defination Ends  ################
     #################### Purchase method for player Starts  ######################
     if operation == "purchase":
-        assert (len(args) == 3)
+        Require(len(args) == 3)
         account = args[0]
         gpId = args[1]
         gpAmount = args[2]
@@ -70,11 +89,11 @@ def Main(operation, args):
     if operation == "getGPMaxPerTx":
         return getGPMaxPerTx()
     if operation == "getGP":
-        assert (len(args) == 1)
+        Require(len(args) == 1)
         gpId = args[0]
         return getGP(gpId)
     if operation == "getGPLeft":
-        assert (len(args) == 1)
+        Require(len(args) == 1)
         gpId = args[0]
         return getGPLeft(gpId)
     #################### Pre-execute methods defination Ends  ######################
@@ -82,7 +101,7 @@ def Main(operation, args):
 
 ############# Methods for Admin account only defination Starts  ################
 def setPropertyHash(propertyReversedHash):
-    assert (CheckWitness(Admin))
+    RequireWitness(Admin)
     Put(GetContext(), PROPERTY_REVERSED_HASH_KEY, propertyReversedHash)
     Notify(["setPropertyHash", propertyReversedHash])
     return True
@@ -96,22 +115,22 @@ def setGP(gpId, gpLimit, price, gpContent):
     :param gpContent: [[tokenId1, amount1], [tokenId2, amount2], ..., [tokenIdN, amountN]]
     :return:
     """
-    assert (CheckWitness(Admin))
+    RequireWitness(Admin)
     gpKey = _concatkey(GP_PREFIX, gpId)
-    assert (not Get(GetContext(), gpKey))
+    Require(not Get(GetContext(), gpKey))
     gpMap = {"price":price}
-    assert (gpLimit > 0)
+    Require(gpLimit > 0)
     content = []
     # ta means [tokenId_n, amount_n]
     for ta in gpContent:
         tokenId = ta[0]
         amount = ta[1]
         # make sure the tokenId is legal
-        assert (tokenId >= 1001 and tokenId <= 999999)
+        Require(tokenId >= 1001 and tokenId <= 999999)
         # make sure the tokenId has been created in property contract <=> name of tokenId is NOT None
         res = DynamicAppCall(getPropertyReversedHash(), "name", [tokenId])
-        assert ( res)
-        assert (amount > 0)
+        Require( res)
+        Require(amount > 0)
         content.append([tokenId, amount])
     contentInfo = Serialize(content)
     gpMap["content"] = contentInfo
@@ -123,8 +142,8 @@ def setGP(gpId, gpLimit, price, gpContent):
     return True
 
 def setGPMaxPerTx(limit):
-    assert (CheckWitness(Admin))
-    assert (limit > 0)
+    RequireWitness(Admin)
+    Require(limit > 0)
     Put(GetContext(), GP_MAX_PER_TX_KEY, limit)
     Notify(["setMaxGPperTx", limit])
     return True
@@ -134,32 +153,32 @@ def withdraw():
     In case someone transfers ong or ont into this contract by accident, Admin can withdraw all the money left in the contract.
     :return:
     """
-    assert (CheckWitness(Admin))
+    RequireWitness(Admin)
     param = state(SelfContractAddress)
     totalOngAmount = Invoke(0, ONGAddress, 'balanceOf', param)
     if totalOngAmount > 0:
-        assert (_tranferNativeAsset(ONGAddress, SelfContractAddress, PreSaleReceiver, totalOngAmount))
+        Require(_tranferNativeAsset(ONGAddress, SelfContractAddress, PreSaleReceiver, totalOngAmount))
     totalOntAmount = Invoke(0, ONTAddress, 'balanceOf', param)
     if totalOntAmount > 0:
-        assert (_tranferNativeAsset(ONTAddress, SelfContractAddress, PreSaleReceiver, totalOntAmount))
+        Require(_tranferNativeAsset(ONTAddress, SelfContractAddress, PreSaleReceiver, totalOntAmount))
     Notify(["withdraw", PreSaleReceiver, totalOngAmount, totalOntAmount])
     return True
 
 def pause():
-    assert (CheckWitness(Admin))
+    RequireWitness(Admin)
     Put(GetContext(), PRESALE_PAUSED_KEY, "T")
     Notify(["pause"])
     return True
 
 def unpause():
-    assert (CheckWitness(Admin))
+    RequireWitness(Admin)
     Put(GetContext(), PRESALE_PAUSED_KEY, "F")
     Notify(["unpause"])
     return True
 
 def migrateContract(code, needStorage, name, version, author, email, description):
-    assert (CheckWitness(Admin))
-    assert (_whenNotPaused() == False)
+    RequireWitness(Admin)
+    Require(_whenNotPaused() == False)
     param = state(SelfContractAddress)
     totalOngAmount = Invoke(0, ONGAddress, 'balanceOf', param)
     totalOntAmount = Invoke(0, ONTAddress, 'balanceOf', param)
@@ -169,10 +188,10 @@ def migrateContract(code, needStorage, name, version, author, email, description
     # assert (res)
 
     # Option2: make sure there is no ong left
-    assert (totalOngAmount == 0 and totalOntAmount == 0)
+    Require(totalOngAmount == 0 and totalOntAmount == 0)
 
     res = Migrate(code, needStorage, name, version, author, email, description)
-    assert (res)
+    Require(res)
     Notify(["migreate"])
     return True
 ############# Methods for Admin account only defination Ends  ################
@@ -190,31 +209,31 @@ def purchase(account, gpId, gpAmount):
     :param gpAmount
     :return:
     """
-    assert (_whenNotPaused())
-    assert (CheckWitness(account))
+    Require(_whenNotPaused())
+    RequireWitness(account)
     gpMaxPerTx = getGPMaxPerTx()
     if not gpMaxPerTx:
-        assert (gpAmount > 0)
+        Require(gpAmount > 0)
     else:
-        assert (gpAmount > 0 and gpAmount <= gpMaxPerTx)
+        Require(gpAmount > 0 and gpAmount <= gpMaxPerTx)
     # make sure there are enough available gift packages left
     gpLeft = getGPLeft(gpId)
-    assert (gpLeft >= gpAmount)
+    Require(gpLeft >= gpAmount)
     # get the gift package info
     priceContent = getGP(gpId)
-    assert (len(priceContent) == 2)
+    Require(len(priceContent) == 2)
     price = priceContent[0]
     content = priceContent[1]
     # transfer ONG from account to the contract
     ongToBeTransferred = price * gpAmount
-    assert (_tranferNativeAsset(ONGAddress, account, PreSaleReceiver, ongToBeTransferred))
+    Require(_tranferNativeAsset(ONGAddress, account, PreSaleReceiver, ongToBeTransferred))
     # mint all the tokens within the gpId gift package.
     argsForMultiMintToken = []
     for ta in content:
         tokenId = ta[0]
         amount = ta[1] * gpAmount
         argsForMultiMintToken.append([account, tokenId, amount])
-    assert (DynamicAppCall(getPropertyReversedHash(), "multiMintToken", argsForMultiMintToken))
+    Require(DynamicAppCall(getPropertyReversedHash(), "multiMintToken", argsForMultiMintToken))
     Put(GetContext(), _concatkey(GP_LEFT_PREFIX, gpId), gpLeft - gpAmount)
     Notify(["purchase", account, gpId, price, gpAmount])
     return True
